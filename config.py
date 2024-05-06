25a453d896746dea0bdf8f1ef4b6ec0c58a5e170
gluoneros <gluoneros@gmail.com>
2024-05-06 16:38:05 -0500

    actualy

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 
 load_dotenv()
 
-user = os.environ["MYSQL_USER"]
-password = os.environ["MYSQL_PASSWORD"]
-host = os.environ["MYSQL_HOST"]
-database = os.environ["MYSQL_DATABASE"]
+user = os.environ['usuario1']
+password = os.environ["usuario6"]
+host = os.environ["127.0.0.1"]
+database = os.environ["contactsdb"]
 
 DATABASE_CONNECTION_URI = f'mysql://{user}:{password}@{host}/{database}'
 print(DATABASE_CONNECTION_URI)
