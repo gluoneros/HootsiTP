47ddfc48fa3acad89f4d5b2fd2490706bf6e54a2
gluoneros <gluoneros@gmail.com>
2024-05-05 23:44:06 -0500

    actual

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, jsonify
+from flask import Flask
+from routes.contacts import contacts
+from flask_sqlalchemy import SQLAlchemy
+from config import DATABASE_CONNECTION_URI
 
 app = Flask(__name__)
 
-@app.route("/hola", methods=['GET'])
-def hola():
-    return jsonify({'message':"Endpoint desde hola"})
+# settings
+app.secret_key = 'mysecret'
+print(DATABASE_CONNECTION_URI)
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_CONNECTION_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# no cache
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+SQLAlchemy(app)
+
+app.register_blueprint(contacts)
