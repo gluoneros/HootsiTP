39402ecdc9c7101ce4147110f254226f3b4d7b54
gluoneros <gluoneros@gmail.com>
2024-05-08 23:02:46 -0500

    change

--- app.py
+++ app.py
@@ -1,19 +1,7 @@
 from flask import Flask
-from routes.contacts import contacts
-from flask_sqlalchemy import SQLAlchemy
-from config import DATABASE_CONNECTION_URI
+from routes.inventory import inventory
 
 app = Flask(__name__)
 
-# settings
-app.secret_key = 'mysecret'
-print(DATABASE_CONNECTION_URI)
-app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_CONNECTION_URI
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-# no cache
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-
-SQLAlchemy(app)
-
-app.register_blueprint(contacts)
+app.register_blueprint(inventory)
---
+++ routes/inventory.py
@@ -0,0 +1,19 @@
+from flask import Blueprint
+
+inventory = Blueprint('inventory', __name__)
+
+@inventory.route('/')
+def shows():
+    return 'Shows all inventory items.'
+
+@inventory.route('/add')
+def add():
+    return 'Adds a new item to the database'
+
+@inventory.route('/edit')
+def edit():
+    return 'Edits an existing item using its ID'
+
+@inventory.route('/delete')
+def delete():
+    return 'Deletes an item using its ID.'
