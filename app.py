3da973afa96f40203b2e0b5663600b436e10058e
gluoneros <gluoneros@gmail.com>
2024-07-02 16:32:48 -0500

    a punto

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ db = SQLAlchemy(app)
 
 class Inventory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(80), nullable=False)
     price = db.Column(db.Float, nullable=False)
     mac_address = db.Column(db.String(17), nullable=False)
     serial_number = db.Column(db.String(50), nullable=False)
-    manufacturer = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.Text, nullable=True)
+    manufacturer = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(200), nullable=True)
 
     def __repr__(self):
         return f"Inventory(id={self.id}, name={self.name}, price={self.price}, mac_address={self.mac_address}, serial_number={self.serial_number}, manufacturer={self.manufacturer}, description={self.description})"
@@ -22,10 +22,10 @@ class Inventory(db.Model):
 #def home():
 #    return "hello"    
 
-@app.route('/Inventory', methods=['GET', 'POST'])
-def handle_inventory():
+@app.route('/add', methods=['GET', 'POST'])
+def add():
     if request.method == 'POST':
-        data = request.json
+        data = request.form
         new_item = Inventory(
             name=data['name'],
             price=data['price'],
--- routes/routes.py
+++ routes/routes.py
@@ -9,9 +9,35 @@ inventory = Blueprint('inventory', __name__)
 def shows():
     return render_template('index.html')
 
-@inventory.route('/add')
+@inventory.route('/add', methods=['GET', 'POST'])
 def add():
-    return 'Adds a new item to the database.'
+    if request.method == 'POST':
+        data = request.json
+        new_item = Inventory(
+            name=data['name'],
+            price=data['price'],
+            mac_address=data['mac_address'],
+            serial_number=data['serial_number'],
+            manufacturer=data['manufacturer'],
+            description=data.get('description')
+        )
+        db.session.add(new_item)
+        db.session.commit()
+        return jsonify({"message": "Item added successfully"}), 201
+    elif request.method == 'GET':
+        inventory_items = Inventory.query.all()
+        result = [
+            {
+                "id": item.id,
+                "name": item.name,
+                "price": item.price,
+                "mac_address": item.mac_address,
+                "serial_number": item.serial_number,
+                "manufacturer": item.manufacturer,
+                "description": item.description
+            } for item in inventory_items
+        ]
+        return jsonify(result), 200
 
 @inventory.route('/edit')
 def edit():
